bfc7c175047bc0db0bf329cff92bd156875a0750
data614 <contact@personapath.com.au>
2025-09-23 12:17:54 +1000

    Add job intelligence API backend

---
+++ server.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const path = require('path');
+const express = require('express');
+const { createJobIntelRouter } = require('./api/job-intel');
+
+const app = express();
+const PORT = Number(process.env.PORT) || 3000;
+
+app.use(express.json({ limit: '1mb' }));
+app.use('/api/job-intel', createJobIntelRouter());
+
+app.use(express.static(path.join(__dirname)));
+
+app.use((err, req, res, next) => {
+  // eslint-disable-next-line no-console
+  console.error('Unhandled error in API', err);
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(500).json({ error: 'Internal server error' });
+});
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Pathfinder server listening on port ${PORT}`);
+});
